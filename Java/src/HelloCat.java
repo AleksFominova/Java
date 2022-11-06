6c63398665a503f8830d198c3f803831dd122865
AleksFominova <111460743+AleksFominova@users.noreply.github.com>
2022-11-06 13:58:18 -0800

    HW1

---
+++ Java/src/HelloCat.java
@@ -0,0 +1,17 @@
+/**
+ * Java.HW #1
+ *
+ * @author Aleksandra
+ *
+ * @version 31.10 - 06.11
+ */
+public class HelloCat {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik","red", 4);
+        cat.voice();
+        System.out.println(cat);
+
+
+    }
+}
+
